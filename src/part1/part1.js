5cf493077f3c11c5be34d41ab9e05d45fdfa3b14
jiafa.li <247686992@qq.c247686992@qq.comom>
2019-05-21 19:02:45 +0800

    v1.0.0-dev

---
+++ src/part1/part1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import heisiyeti from '../assets/heiseyeti.png';
+import './part1.css';
+
+function Part1() {
+  return (
+    <div className="part1">
+      <div className="black-juice">
+        <img src={heisiyeti} alt=""/>
+      </div>
+    </div>
+  );
+}
+
+export default Part1;
